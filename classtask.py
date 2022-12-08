8f958dc9a6b652322cd54a2d0f8b507393f2aa11
Rasmita Gautam <rasmitagautam13@gmail.com>
2022-12-08 14:56:40 +0545

    uncomment neccesary lines of code

--- classtask.py
+++ classtask.py
@@ -1,24 +1,24 @@
-# students_score = [
-#     {"name": "Ram", "score": 80},
-#     {"name": "Sita", "score": 100},
-#     {"name": "Abc", "score": 35},
-#     {"name": "xyz", "score": 40},
-#     {"name": "john", "score": 37},
-#     {"name": "shyam", "score": 90},
-#     {"name": "hari", "score": 36},
-# ]
-# output = []
-# for score in students_score:
-#     a = score.get("score")
+students_score = [
+    {"name": "Ram", "score": 80},
+    {"name": "Sita", "score": 100},
+    {"name": "Abc", "score": 35},
+    {"name": "xyz", "score": 40},
+    {"name": "john", "score": 37},
+    {"name": "shyam", "score": 90},
+    {"name": "hari", "score": 36},
+]
+output = []
+for score in students_score:
+    a = score.get("score")
    
-#     if a>=40:
-#         output.append(score)
-# print(output)
+    if a>=40:
+        output.append(score)
+print(output)
 
 
 
-# output = list(filter(lambda i:i.get("score")>= 40, students_score))
-# print(output)
+output = list(filter(lambda i:i.get("score")>= 40, students_score))
+print(output)
 
 
 
